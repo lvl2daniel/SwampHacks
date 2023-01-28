c06558aae2e937f2c112c41c13e89c01ea263acd
Daniel Gonzalez <Danielgonzalez4444@yahoo.com>
2023-01-28 02:14:47 -0500

    Add Shadows

--- main.js
+++ main.js
@@ -22,31 +22,42 @@ const camera = new THREE.
     renderer.setSize(window.innerWidth*1.5, window.innerHeight*1.5);
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setClearAlpha(100);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    const light = new THREE.DirectionalLight( 0xffffff, 1 );
+    light.position.set( -.45, .67, .80 ); //default; light shining from top
+    light.castShadow = true; // default false
+    scene.add( light );
+    light.shadow.mapSize.width = 512; // default
+    light.shadow.mapSize.height = 512; // default
+    light.shadow.camera.near = 0.5; // default
+    light.shadow.camera.far = 500; // default
     document.body.appendChild(renderer.
         domElement);
     const plane = new THREE.Mesh(
         new THREE.PlaneGeometry(150,150,1000,1000),
-        new THREE.MeshBasicMaterial({
+        new THREE.MeshStandardMaterial({
             map: new THREE.TextureLoader().load('starz.jpg')
         })
     )
+    plane.receiveShadow = false;
     const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(8, 900, 900),
-    new THREE.MeshBasicMaterial({
+    new THREE.MeshStandardMaterial({
         map: new THREE.TextureLoader().load('16kearth.jpg')
+        
     }))
 
     scene.add(sphere);
     scene.add(plane);
     console.log(plane);
-    camera.position.z = 20
+    camera.position.z = 25
     console.log(sphere);
     function animate() {
         requestAnimationFrame(animate);
         renderer.render(scene, camera);
-        sphere.rotation.x += .0005;
-        sphere.rotation.y += .001;
+        //sphere.rotation.x += .0005;
+        //sphere.rotation.y += .001;
     }
     animate();
-
 
