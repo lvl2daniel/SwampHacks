44d78505122e158bfcd504508c8b3d7f31321cad
Daniel Gonzalez <Danielgonzalez4444@yahoo.com>
2023-01-28 01:22:17 -0500

    Add stars and stuff

--- main.js
+++ main.js
@@ -19,25 +19,32 @@ const camera = new THREE.
     console.log(scene);
     console.log(camera);
     console.log(renderer);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth*1.5, window.innerHeight*1.5);
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setClearAlpha(100);
     document.body.appendChild(renderer.
         domElement);
-
+    const plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(150,150,1000,1000),
+        new THREE.MeshBasicMaterial({
+            map: new THREE.TextureLoader().load('starz.jpg')
+        })
+    )
     const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(5, 90, 90),
+    new THREE.SphereGeometry(8, 900, 900),
     new THREE.MeshBasicMaterial({
         map: new THREE.TextureLoader().load('16kearth.jpg')
     }))
 
     scene.add(sphere);
-    camera.position.z = 15
+    scene.add(plane);
+    console.log(plane);
+    camera.position.z = 20
     console.log(sphere);
     function animate() {
         requestAnimationFrame(animate);
         renderer.render(scene, camera);
-        sphere.rotation.x += .00005;
+        sphere.rotation.x += .0005;
         sphere.rotation.y += .001;
     }
     animate();
