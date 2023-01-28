ca95b63ecc8914ed8f92998c6837f4b21d8e6e47
Evan Goldsmith <evangoldsmith1@gmail.com>
2023-01-28 17:54:56 -0500

    Fix spacing

--- main.js
+++ main.js
@@ -10,60 +10,63 @@ async function update() {
     arr = await appendSatArray();
     console.log(arr);
 }
+
 const scene = new THREE.Scene();
 const camera = new THREE.
-    PerspectiveCamera(
-        75,
-        window.innerWidth/2 / window.innerHeight,
-        0.1,
-        1000
-    );
-    const renderer = new THREE.WebGLRenderer(
-        {
-            antialias: true,
-            canvas: document.querySelector('canvas')
-        }
-    );
-    const canvasContainer = document.querySelector('#canvasContainer')
-    scene.background = new THREE.TextureLoader().load('./img/starz.jpg');
-    console.log(scene);
-    console.log(camera);
-    console.log(renderer);
-    renderer.setSize(canvasContainer.offsetWidth, canvasContainer.offsetHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setClearAlpha(100);
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-    const light = new THREE.AmbientLight( 0xCFD1CC);
-    scene.add( light );
-    
-    const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(5, 100, 100),
-    new THREE.MeshStandardMaterial({
-        map: new THREE.TextureLoader().load('./img/16kearth.jpg')
-        
-    }))
-    scene.add(sphere);
-    function createObject(xCoord, yCoord, zCoord, name){
-        const newSattelite = new THREE.Mesh(
-            new THREE.ConeGeometry(.1, .05, 3, 1),
-            new THREE.MeshBasicMaterial({color: 0xff0000})
-        )
-        const a = new THREE.Vector3(3, 3, 3)
-        newSattelite.translateX(xCoord);
-        newSattelite.translateY(yCoord);
-        newSattelite.translateZ(zCoord);
-        name = newSattelite;
-        console.log(name);
-        scene.add(name);
+PerspectiveCamera(
+    75,
+    window.innerWidth/2 / window.innerHeight,
+    0.1,
+    1000
+);
+const renderer = new THREE.WebGLRenderer(
+    {
+        antialias: true,
+        canvas: document.querySelector('canvas')
     }
-    camera.position.z = 10
-    console.log(sphere);
-    const controls = new OrbitControls(camera, renderer.domElement)
-    controls.enableDamping = false;
-    controls.dampingFactor = .5;
-    controls.enablePan = false;
-    createObject(4, 4, 3.3, 3, 'newSat');
+);
+
+const canvasContainer = document.querySelector('#canvasContainer')
+scene.background = new THREE.TextureLoader().load('./img/starz.jpg');
+console.log(scene);
+console.log(camera);
+console.log(renderer);
+renderer.setSize(canvasContainer.offsetWidth, canvasContainer.offsetHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setClearAlpha(100);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+const light = new THREE.AmbientLight( 0xCFD1CC);
+scene.add( light );
+
+const sphere = new THREE.Mesh(
+new THREE.SphereGeometry(5, 100, 100),
+new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load('./img/16kearth.jpg')
+}));
+scene.add(sphere);
+
+function createObject(xCoord, yCoord, zCoord, name){
+    const newSattelite = new THREE.Mesh(
+        new THREE.ConeGeometry(.1, .05, 3, 1),
+        new THREE.MeshBasicMaterial({color: 0xff0000})
+    )
+    const a = new THREE.Vector3(3, 3, 3)
+    newSattelite.translateX(xCoord);
+    newSattelite.translateY(yCoord);
+    newSattelite.translateZ(zCoord);
+    name = newSattelite;
+    console.log(name);
+    scene.add(name);
+}
+
+camera.position.z = 10
+console.log(sphere);
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = false;
+controls.dampingFactor = .5;
+controls.enablePan = false;
+createObject(4, 4, 3.3, 3, 'newSat');
 
 function animate() {
     requestAnimationFrame(animate);
